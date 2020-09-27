70eb709c9df7829211888f7688d17c7d86a813c3
Stephen Miller <stephen.miller7@gmail.com>
2020-09-26 21:06:19 -0400

    added commants and spaces for readability

--- ego_problem.py
+++ ego_problem.py
@@ -1,12 +1,18 @@
+#start input
 name=input("What is your name? ")
 egoinput=input("How big is your ego? ")
+
+#start output
+#header
 ego=egoinput.lower()
 f= open("your_ego.txt", 'w+')
 x="User's name: "+name+"\n"
 namereveresed=name[::-1]
 x=x+"User's name reversed: "+namereveresed+"\n"
 x=x+"User's ego: "+egoinput+"\n"
+#separator
 x=x+"*******************"+"\n"
+#body
 if ego == "small":
 	x=x+"1 "+name+"\n"
 elif ego == "medium":
@@ -17,6 +23,10 @@ elif ego == "large":
 		x=x+str(y)+" "+name+"\n"
 else: 
 	x=x+"User has no ego" 
+
+#print to file
 print(x,file=f)	
+
+#print to screen
 print(x)
 f.close
